a23768e59a94c8b65701f3c16f4a8c760be842a1
OleksanraDubska <dubskaoleksandra@gmail.com>
2022-11-17 23:55:40 +0200

    Виконано завдання

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+const galleryCards = galleryItems.map(({ preview, original, description }) =>
+        `<div class="gallery__item">
+        <a class="gallery__link" href=${original}>
+          <img
+            class="gallery__image"
+            src=${preview}
+            data-source=${original}
+            alt=${description}
+          />
+        </a>
+        </div>`
+        ).join('');
+
+galleryRef.innerHTML = galleryCards;
+
+galleryRef.addEventListener('click', onModalOpen);
+
+function onModalOpen(event) {
+    event.preventDefault();
+
+    if (event.target.nodeName !== "IMG") {
+        return
+    }
+
+    const modal = basicLightbox.create(`
+        <div class="modal">
+            <img src="${event.target.dataset.source}">
+        </div>
+         `,
+        {
+            onShow: (modal) => {
+                modal.element().onclick = modal.close;
+            }
+        },
+    );
+
+    modal.show();
+
+    function onModalClose(event) {
+        if (event.code !== "Escape") {
+            return;
+        }
+
+        modal.close();
+    }
+
+    window.addEventListener('keydown', onModalClose);
+}
+    
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+const galleryCards = galleryItems.map(({ preview, original, description }) =>
+        `<a class="gallery__item" href=${original}>
+            <img class="gallery__image" src=${preview} alt=${description} />
+        </a>`
+        ).join('');
+
+galleryRef.innerHTML = galleryCards;
+
+let modal = new SimpleLightbox('.gallery a', { 
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+
+galleryRef.addEventListener('click', onModalOpen);
+
+function onModalOpen(event) {
+    event.preventDefault();
+
+    if (event.target.nodeName !== "IMG") {
+        return
+    }
+}
